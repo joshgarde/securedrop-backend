a9b505ae59ca70af40572e4828c52f8f0a900e4e
NatalieDinh <NatalieDinh@users.noreply.github.com>
2021-01-15 17:21:09 -0800

    added upload to s3 bucket

--- Project/src/main/java/com/CrimsonGlory/dropbox/MainController.java
+++ Project/src/main/java/com/CrimsonGlory/dropbox/MainController.java
@@ -5,14 +5,22 @@ import com.CrimsonGlory.dropbox.Database.FileRepository;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+import java.io.IOException;
 
 @RestController
 public class MainController {
 
     private DynamoDBMapper dynamoDBMapper;
+    private String awsBucket = "securedrop-ncali";
 
     @Autowired
     private AmazonDynamoDB amazonDynamoDB;
@@ -31,5 +39,23 @@ public class MainController {
         FileInfo info = new FileInfo(metadata, authtext);
         repository.save(info);
         return info.getId();
+    }
+
+    public String uploadToS3(MultipartFile file){
+        Region region = Region.US_WEST_1;
+        try(S3Client s3 = S3Client.builder()
+                .region(region)
+                .build()){
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(awsBucket)
+                    .key(String.valueOf(file))
+                    .build();
+            PutObjectResponse response = s3.putObject(request, RequestBody.fromBytes(file.getBytes()));
+            return response.eTag();
+
+        } catch (IOException | S3Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "Error uploading file.";
     }
 }
